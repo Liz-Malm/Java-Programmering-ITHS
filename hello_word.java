1359b46fed9d516da876d8976e066d3ec155b1cb
”Liz-Malm” <Elizabeth.malm@iths.se>
2025-09-15 14:41:30 +0200

    Made this script to test the upload function

---
+++ hello_word.java
@@ -0,0 +1,10 @@
+public class hello_word {
+    public static void main(String[] args) {
+       
+        String helloWorld = "Hello World";
+        System.out.println(helloWorld);
+
+        
+
+    }
+}
