9925d14b8b8bc4fac699a9002971e643f48ce036
”Liz-Malm” <Elizabeth.malm@iths.se>
2025-09-22 10:12:57 +0200

    fixing the upload

--- My_Programs/Lesson_03/Unicorn.java
+++ My_Programs/Lesson_03/Unicorn.java
@@ -1,10 +1,10 @@
 package my_programs.lesson_03;
-public class Unicorn {
+public class unicorn {
     int age;
     String color, mood;
     boolean horn, alive;
 
-    Unicorn(int age, String color, String mood, boolean horn, boolean alive) {
+    unicorn(int age, String color, String mood, boolean horn, boolean alive) {
         this.age = age;
         this.color = color;
         this.mood = mood;
